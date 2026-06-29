3e7ee382496e791af1ca631b760b3e0244e55175
sinthy <sinthiamst8@gmail.com>
2026-06-29 10:26:30 +0600

    Create superkeywardConstructor.java

---
+++ superkeywardConstructor.java
@@ -0,0 +1,19 @@
+public class siam{
+public siam(){
+    System.out.println("i am eating rice");
+}
+}
+class abir extends siam{
+public abir(){
+super();
+    System.out.println("he is eating gaza");
+}
+ }
+  class taster{
+public static void main (String[]args){
+    abir a1=new abir();
+    
+}
+
+
+  }
